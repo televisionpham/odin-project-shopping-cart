fbf2d1616e59cd178995f087dc282c85cc787613
Van Pham <televisionpham@gmail.com>
2023-01-18 23:47:16 +0700

    added dummy products

---
+++ src/pages/CONSTANTS.js
@@ -0,0 +1,74 @@
+export const products = [
+  {
+    name: "neque",
+    image: "./assets/01.png",
+    price: 1,
+    description: "Neque porro quisquam est qui dolorem",
+  },
+  {
+    name: "porro",
+    image: "./assets/02.png",
+    price: 2,
+    description: "Neque porro quisquam est qui dolorem",
+  },
+  {
+    name: "quisquam",
+    image: "./assets/03.jpg",
+    price: 3,
+    description: "Neque porro quisquam est qui dolorem",
+  },
+  {
+    name: "est",
+    image: "./assets/04.jpg",
+    price: 4,
+    description: "Neque porro quisquam est qui dolorem",
+  },
+  {
+    name: "qui",
+    image: "./assets/05.png",
+    price: 5,
+    description: "Neque porro quisquam est qui dolorem",
+  },
+  {
+    name: "dolorem",
+    image: "./assets/06.png",
+    price: 6,
+    description: "Neque porro quisquam est qui dolorem",
+  },
+  {
+    name: "ipsum",
+    image: "./assets/07.jpg",
+    price: 7,
+    description: "Neque porro quisquam est qui dolorem",
+  },
+  {
+    name: "quia",
+    image: "./assets/08.jpg",
+    price: 8,
+    description: "Neque porro quisquam est qui dolorem",
+  },
+  {
+    name: "dolor",
+    image: "./assets/09.jpg",
+    price: 9,
+    description: "Neque porro quisquam est qui dolorem",
+  },
+  {
+    name: "sit",
+    image: "./assets/10.jpg",
+    price: 10,
+    description: "Neque porro quisquam est qui dolorem",
+  },
+  {
+    name: "amet",
+    image: "./assets/11.jpg",
+    price: 11,
+    description: "Neque porro quisquam est qui dolorem",
+  },
+  {
+    name: "consectetur",
+    image: "./assets/12.jpg",
+    price: 12,
+    description: "Neque porro quisquam est qui dolorem",
+  },
+];
